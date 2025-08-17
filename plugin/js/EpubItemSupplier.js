9b1591afa9d160a1b947cb8cf1eea31059ed0cd7
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2025-08-17 17:22:13 +0000

    Update EpubItemSupplier.js

--- plugin/js/EpubItemSupplier.js
+++ plugin/js/EpubItemSupplier.js
@@ -40,12 +40,19 @@ class EpubItemSupplier { // eslint-disable-line no-unused-vars
     }
 
     makeCoverImageXhtmlFile(emptyDocFactory) {
-        let doc = emptyDocFactory();
-        let body = doc.getElementsByTagName("body")[0];
-        let userPreferences = this.imageCollector.userPreferences;
-        body.appendChild(this.coverImageInfo.createImageElement(userPreferences));
-        return util.xmlToString(doc);
+    let doc = emptyDocFactory();
+    let body = doc.getElementsByTagName("body");
+    let userPreferences = this.imageCollector.userPreferences;
+    
+    // Set cover page title
+    let title = doc.querySelector("title");
+    if (title && util.isNullOrEmpty(title.textContent)) {
+        title.textContent = "Cover";
     }
+    
+    body.appendChild(this.coverImageInfo.createImageElement(userPreferences));
+    return util.xmlToString(doc);
+}
 
     hasCoverImageFile() {
         return (this.coverImageInfo != null);
